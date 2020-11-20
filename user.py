2edf237324f2cbf29ad156099fd4c251ada69a66
philipiaeveline <philipiaeveline13@gmail.com>
2020-11-20 21:21:32 +0300

    add

--- user.py
+++ user.py
@@ -2,7 +2,7 @@ class User:
     """
     a class that generates new instances of users
     """
-    pass
+   
     users_array = []
     def __init__(self, first_name, last_name, phone_number, email):
         self.first_name = first_name
