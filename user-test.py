f3378e9c0992485ad6629a1564b0812ddc8333d9
philipiaeveline <philipiaeveline13@gmail.com>
2020-11-21 10:43:00 +0300

    add

--- user-test.py
+++ user-test.py
@@ -34,66 +34,110 @@ class TestUser(unittest.TestCase):
      tearDown method that does clean up after each test case has run.
      '''
 
-            User.user_list = []
+     User.user_list = []
 
 
     def test_save_multiple_users(self):
 
-        '''
-        test_save_multiple_users to check if we can save multiple user
-        objects to our user_list
-        '''
+     '''
+     test_save_multiple_users to check if we can save multiple user
+     objects to our user_list
+     '''
 
-        self.new_user.save_user()
-        test_user = User("John", "Wick", "JWick", "dontkillmydog")
-        test_user.save_user()
-
-        self.assertEqual(len(User.user_list),2)
-
+     self.new_user.save_user()
+     test_user = User("John", "Wick", "JWick", "dontkillmydog")
+     test_user.save_user()
+      self.assertEqual(len(User.user_list),2)
 
     def test_delete_user(self):
-        '''
-        test_delete_user to test if we van remove a user from our user list
-        '''
+     '''
+     test_delete_user to test if we van remove a user from our user list
+     '''
 
-        self.new_user.save_user()
-        test_user = User("Abraham", "Lincon", "ALincon", "americaisgreat")
-        test_user.save_user()
+     self.new_user.save_user()
+     test_user = User("Abraham", "Lincon", "ALincon", "americaisgreat")
+     test_user.save_user()
 
-        self.new_user.delete_user() #Deleting a user object
-        self.assertEqual(len(User.user_list),1)
+     self.new_user.delete_user() #Deleting a user object
+     self.assertEqual(len(User.user_list),1)
 
 
     def test_find_user_by_username(self):
-        '''
-        test to check if we can find a user by their username and display infformation
-        '''
+     '''
+     test to check if we can find a user by their username and display infformation
+     '''
 
-        self.new_user.save_user()
-        test_user = User("Abraham", "Lincon", "ALincon", "americaisgreat")
-        test_user.save_user()
+     self.new_user.save_user()
+     test_user = User("Abraham", "Lincon", "ALincon", "americaisgreat")
+     test_user.save_user()
 
-        found_user = User.find_by_username("ALincon")
-        self.assertEqual(found_user.user_name,"ALincon")
+     found_user = User.find_by_username("ALincon")
+     self.assertEqual(found_user.user_name,"ALincon")
 
     def test_find_user_by_password(self):
-        '''
-        test to check if we can find a user by their password
-        '''
+      '''
+     test to check if we can find a user by their password
+     '''
 
-        self.new_user.save_user()
-        test_user = User("Mark", "Cuban", "MCuban", "moneyisgood")
-        test_user.save_user()
+     self.new_user.save_user()
+     test_user = User("Mark", "Cuban", "MCuban", "moneyisgood")
+     test_user.save_user()
 
-        found_password = User.find_by_userpassword("moneyisgood")
-        self.assertEqual(found_password.password,"moneyisgood")
+     found_password = User.find_by_userpassword("moneyisgood")
+     self.assertEqual(found_password.password,"moneyisgood")
 
     def test_display_user_information(self):
-        '''
-        test to check if we can be able to display users saved in user_list
-        '''
+      '''
+      test to check if we can be able to display users saved in user_list
+      '''
 
-        self.assertEqual(User.display_userInfo(),User.user_list)
+     self.assertEqual(User.display_userInfo(),User.user_list)
+
+
+
+  def test_delete_user(self):
+     '''
+     test_delete_user to test if we van remove a user from our user list
+     '''
+
+     self.new_user.save_user()
+      test_user = User("Abraham", "Lincon", "ALincon", "americaisgreat")
+     test_user.save_user()
+
+     self.new_user.delete_user() #Deleting a user object
+     self.assertEqual(len(User.user_list),1)
+
+
+    def test_find_user_by_username(self):
+     '''
+     test to check if we can find a user by their username and display infformation
+     '''
+
+     self.new_user.save_user()
+     test_user = User("Abraham", "Lincon", "ALincon", "americaisgreat")
+     test_user.save_user()
+
+     found_user = User.find_by_username("ALincon")
+     self.assertEqual(found_user.user_name,"ALincon")
+
+    def test_find_user_by_password(self):
+     '''
+     test to check if we can find a user by their password
+     '''
+
+     self.new_user.save_user()
+     test_user = User("Mark", "Cuban", "MCuban", "moneyisgood")
+     test_user.save_user()
+
+     found_password = User.find_by_userpassword("moneyisgood")
+     self.assertEqual(found_password.password,"moneyisgood")
+
+    def test_display_user_information(self):
+     '''
+     test to check if we can be able to display users saved in user_list
+     '''
+
+     self.assertEqual(User.display_userInfo(),User.user_list)
 
 
 
@@ -102,38 +146,38 @@ class TestUser(unittest.TestCase):
 class TestAccount(unittest.TestCase):
     def setUp(self):
 
-        self.new_account = Account("Instagram", "Jack_Sparrow", "apirateslife")
+     self.new_account = Account("Instagram", "Jack_Sparrow", "apirateslife")
 
     def test_init(self):
-        self.assertEqual(self.new_account.account_name,"Instagram")
-        self.assertEqual(self.new_account.account_userName,"Jack_Sparrow")
-        self.assertEqual(self.new_account.account_password,"apirateslife")
+     self.assertEqual(self.new_account.account_name,"Instagram")
+     self.assertEqual(self.new_account.account_userName,"Jack_Sparrow")
+     self.assertEqual(self.new_account.account_password,"apirateslife")
 
     def test_save_account(self):
-        self.new_account.save_account() #saving the new account
-        self.assertEqual(len(Account.account_list),1)
+     self.new_account.save_account() #saving the new account
+     self.assertEqual(len(Account.account_list),1)
 
     def tearDown(self):
-        Account.account_list = []
+     Account.account_list = []
 
     def test_display_account_credentials(self):
-        self.assertEqual(Account.display_accounts(),Account.account_list)
+     self.assertEqual(Account.display_accounts(),Account.account_list)
 
     def test_delete_account(self):
-        self.new_account.save_account()
-        test_account = Account("Twitter", "James_Lebron", "basketballislife")
-        test_account.save_account()
+     self.new_account.save_account()
+     test_account = Account("Twitter", "James_Lebron", "basketballislife")
+     test_account.save_account()
 
-        test_account.delete_account()
-        self.assertEqual(len(Account.account_list),1)
+     test_account.delete_account()
+     self.assertEqual(len(Account.account_list),1)
 
     def test_find_account_by_name(self):
-        self.new_account.save_account()
-        test_account = Account("Medium", "Jeff_Bezos", "investingissmart")
-        test_account.save_account()
+     self.new_account.save_account()
+     test_account = Account("Medium", "Jeff_Bezos", "investingissmart")
+     test_account.save_account()
 
-        found_account = Account.find_by_accountName("Medium")
-        self.assertEqual(found_account.account_name,"Medium")
+     found_account = Account.find_by_accountName("Medium")
+     self.assertEqual(found_account.account_name,"Medium")
 
 
 
