bcbafd6d5a93bab144157c49be0d5eb23dc1c9ef
philipiaeveline <philipiaeveline13@gmail.com>
2020-11-21 10:27:04 +0300

    add

--- user-test.py
+++ user-test.py
@@ -4,34 +4,35 @@ from user import Account
 
 class TestLocker(unittest.TestCase):
 class TestUser(unittest.TestCase):
-    '''
-    Test class that defines test cases for the user class behaviours.
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
-    '''
+     '''
+     Test class that defines test cases for the user class behaviours.
+     Args:
+     unittest.TestCase: TestCase class that helps in creating test cases
+     '''
     def setUp(self):
-        '''
-        Set up method to run before each test cases.
-        '''
-        self.new_user = User("Henry", "Porter", "HPorter", "porterwashere")
+
+     '''
+     Set up method to run before each test cases.
+     '''
+     self.new_user = User("Henry", "Porter", "HPorter", "porterwashere")
 
     def test_init(self):
-@@ -11,6 +21,121 @@ def test_init(self):
-        self.assertEqual(self.new_user.user_name, "HPorter")
-        self.assertEqual(self.new_user.password, "porterwashere")
+     @@ -11,6 +21,121 @@ def test_init(self):
+     self.assertEqual(self.new_user.user_name, "HPorter")
+     self.assertEqual(self.new_user.password, "porterwashere")
 
     def test_save_user(self):
-        '''
-        test_save_user test case to test if the user object is saved into the user listed
-        '''
-        self.new_user.save_user() #saving the new user_list
-        self.assertEqual(len(User.user_list),1)
+     '''
+     test_save_user test case to test if the user object is saved into the user listed
+     '''
+     self.new_user.save_user() #saving the new user_list
+     self.assertEqual(len(User.user_list),1)
 
 
     def tearDown(self):
-            '''
-            tearDown method that does clean up after each test case has run.
-            '''
+     '''
+     tearDown method that does clean up after each test case has run.
+     '''
 
             User.user_list = []
 
